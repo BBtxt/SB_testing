699f12805fb268fc39657d29300349191fc31b06
Brando <bbaker.sh@gmail.com>
2023-11-05 11:49:50 -0800

    added test for loan calculation

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,28 @@
+describe("Calculator", () => {
+  it("should calculate the monthly rate correctly", function () {
+    const values = {
+      amount: 10000,
+      years: 8,
+      rate: 5.5,
+    };
+    expect(calculateMonthlyPayment(values)).toEqual("128.99");
+  });
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
+  it("should return a result with 2 decimal places", function () {
+    const values = {
+      amount: 10000,
+      years: 8,
+      rate: 5.5,
+    };
+    expect(calculateMonthlyPayment(values)).not.toEqual("128.999");
+  });
+
+  it("Should be able to receive a special rate of 0", () => {
+    const values = {
+      amount: 10000,
+      years: 8,
+      rate: 0,
+    };
+    expect(calculateMonthlyPayment(values)).toEqual("104.17");
+  });
 });
-
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
